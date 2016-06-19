b3e5afcd0fd153163060b153d223c4202acd61b1
neo1218 <neo1218@yeah.net>
2016-06-20 00:43:36 +0800

    develop

--- manage.py
+++ manage.py
@@ -6,4 +6,4 @@ from mock.app import mock_app
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'mock':
-        mock_app.run(debug=True)
+        mock_app.run(debug=True, host="47.89.28.131", port=5060)
