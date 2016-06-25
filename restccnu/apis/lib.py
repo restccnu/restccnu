ab288405bfb9bcaf6b969883e7d33ab8df42f7c3
neo1218 <neo1218@yeah.net>
2016-06-25 16:21:55 +0800

    fix lib api

--- restccnu/apis/lib.py
+++ restccnu/apis/lib.py
@@ -11,15 +11,14 @@ from .paginate import _Pagination
 @api.route('/lib/search/')
 @tojson
 def api_search_books():
-    per_page = 5
+    per_page = 20
     keyword = request.args.get('keyword')
     page = int(request.args.get('page') or '1')
     if keyword:
         book_info_list = search_books(keyword)
         pg_book_info_list = _Pagination(book_info_list, page, per_page)
         return {'meta': {
-                'next': pg_book_info_list.next_page,
-                'last': pg_book_info_list.last_page,
+                'max': pg_book_info_list.max_page,
                 'per_page': per_page },
             'results': book_info_list[(page-1)*per_page:page*per_page]}
 
