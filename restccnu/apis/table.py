9eca522b84db722f349beb64d4f93ca1dc917a7d
neo1218 <neo1218@yeah.net>
2016-06-26 01:29:19 +0800

    delete table ✅

--- restccnu/apis/table.py
+++ restccnu/apis/table.py
@@ -18,7 +18,6 @@ from restccnu.models import connection
 def api_get_table(s):
     xnm = request.args.get('xnm')
     xqm = request.args.get('xqm')
-    # sid = request.args.get('sid')
     hashstr = request.headers.get('Authorization')
     base64_hashstr = hashstr[6:]
     id_password = base64.b64decode(base64_hashstr)
@@ -30,8 +29,6 @@ def api_get_table(s):
         u = connection.User()
         u['sid'] = sid; u['table'] = rv
         u.save()
-    # user['table'] = rv
-    # user.save()
     return user['table']
 
 
@@ -39,8 +36,6 @@ def api_get_table(s):
 @require_info_login
 def api_add_table(s):
     if request.method == 'POST':
-        # xnm = request.get_json().get('xnm')
-        # xqm = request.get_json().get('xqm')
         hashstr = request.headers.get('Authorization')
         base64_hashstr = hashstr[6:]
         id_password = base64.b64decode(base64_hashstr)
@@ -56,12 +51,14 @@ def api_add_table(s):
         day = request.get_json().get('day')
         during = request.get_json().get('during')
         place = request.get_json().get('place')
+        id = request.get_json().get('id')
         new_json = {'course': course,
                     'teacher': teacher,
                     'weeks': weeks,
                     'day': day,
                     'during': during,
-                    'place': place}
+                    'place': place,
+                    'id': id}
 
         table = user['table']
         table.append(new_json)
@@ -69,3 +66,25 @@ def api_add_table(s):
         user.save()
 
         return jsonify({}), 201
+
+
+@api.route('/table/<int:id>/', methods=['DELETE'])
+@require_info_login
+def api_delete_table(s, id):
+    hashstr = request.headers.get('Authorization')
+    base64_hashstr = hashstr[6:]
+    id_password = base64.b64decode(base64_hashstr)
+    sid, password = id_password.split(':')
+
+    if request.method == 'DELETE':
+        user = connection.User.find_one({'sid': sid})
+        if user is None:
+            return jsonify({}), 404
+        table = user['table']
+        for i, item in enumerate(table):
+            if item.get('id') == str(id):
+                del table[i]
+        user['table'] = table
+        user.save()
+        return jsonify({}), 200
+
