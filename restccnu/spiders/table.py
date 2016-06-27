38e8dd4cb7f622f91750517e29221e4b07ec4d3c
neo1218 <neo1218@yeah.net>
2016-06-27 20:56:34 +0800

    fix table bug

--- restccnu/spiders/table.py
+++ restccnu/spiders/table.py
@@ -44,10 +44,11 @@ def get_table(s, sid, xnm, xqm):
             weeks = _weeks.split('-')
             _start = int(weeks[0]); _last = int(weeks[-1][:-1])
             weeks_list = range(_start, _last+1)
+        str_weeks_list = [str(i) for i in weeks_list]
         _item_dict = dict({
             'course': item.get('kcmc'),
             'teacher': item.get('xm'),
-            'weeks': weeks_list,
+            'weeks': ','.join(str_weeks_list),
             'day': item.get('xqjmc'),
             'during': item.get('jcs'),
             'place': item.get('xqmc') + item.get('cdmc')})
