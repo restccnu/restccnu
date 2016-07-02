b616b797306a092df2cfbc0cddea44c89e4a1399
neo1218 <neo1218@yeah.net>
2016-07-02 23:09:26 +0800

    成绩查询API✅

--- restccnu/spiders/__init__.py
+++ restccnu/spiders/__init__.py
@@ -27,6 +27,7 @@ link_index_url = "http://portal.ccnu.edu.cn/roamingAction.do?appId=XK"
 
 """成绩查询"""
 grade_index_url = "http://122.204.187.6/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdmKey=N305005&sessionUserKey=%s"
+grade_detail_url = "http://122.204.187.6/cjcx/cjcx_cxCjxq.html?time=1467466507138&gnmkdmKey=N305005&sessionUserKey=%s"
 
 """电费查询"""
 ele_index_url = "http://202.114.38.46/"
