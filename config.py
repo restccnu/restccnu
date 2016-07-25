592fb1593b1db89539dfc5c0e43a48a450a62c8b
neo1218 <neo1218@yeah.net>
2016-07-26 02:28:47 +0800

    set bucket domain

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
 
     QINIU_ACCESS_KEY = os.getenv('QINIU_ACCESS_KEY')
     QINIU_SECRET_KEY = os.getenv('QINIU_SECRET_KEY')
-    QINIU_BUCKET_NAME = os.getenv('QINIU_BUCKET_NAME')
-    QINIU_BUCKET_DOMAIN = os.getenv('QINIU_BUCKET_DOMAIN')
+    QINIU_BUCKET_NAME = os.getenv('QINIU_BUCKET_NAME') or 'ccnustatic'
+    QINIU_BUCKET_DOMAIN = os.getenv('QINIU_BUCKET_DOMAIN') or 'oao7x1n3m.bkt.clouddn.com'
 
 
 config = {
