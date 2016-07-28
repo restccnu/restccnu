b730781b05083881de50d04cf13538457ebc30db
neo1218 <neo1218@yeah.net>
2016-07-29 04:57:27 +0800

    add info api

--- restccnu/apis/__init__.py
+++ restccnu/apis/__init__.py
@@ -29,6 +29,7 @@ def api_index():
                 {'delete personal class': '/api/table/id/'}
             ],
             'Grade Query': '/api/grade/search/?xnm=n&xqm=n',
+            'Info API': '/api/info/'
         },
     }), 200
 
