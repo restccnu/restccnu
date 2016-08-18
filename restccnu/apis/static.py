dbe1f6479245fbb1cc7cebf9657ed53662c5f94a
neo1218 <neo1218@yeah.net>
2016-08-19 07:44:58 +0800

    fix calendar size bug

--- restccnu/apis/static.py
+++ restccnu/apis/static.py
@@ -47,16 +47,14 @@ def get_calendar():
     calendars = eval(rds.get('calendars') or '[]')
     for calendar_dict in calendars:
         filename = calendar_dict.keys()[0]
-        # size = calendar_dict.get(filename)
+        size = calendar_dict.get(filename)
         if filename:
             update_timestamp = qiniu.info(filename).get('putTime')
             json_data.append({
                 'filename': filename,
                 'img': qiniu.url(filename),
                 'update': update_timestamp,
-                # 'size': size
-                'size': qiniu.info(filename).get('fsize'),
-                'info': qiniu.info(filename)
+                'size': size,
             })
         else:
             json_data.append({})
