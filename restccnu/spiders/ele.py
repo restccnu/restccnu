d27237a8f7f29f5c1ca361ba809d8f7578ff3c51
neo1218 <neo1218@yeah.net>
2016-09-08 20:32:44 +0800

    fix ele bug

--- restccnu/spiders/ele.py
+++ restccnu/spiders/ele.py
@@ -46,7 +46,7 @@ def get_ele(meter, dor, typeit):
                 "PartList": "",
                 "SelectPart": 1}
         r = requests.post(ele_url, data=json.dumps(post_data),
-                          headers=headers.update({'Content-Type': 'application/json'}))
+                          headers={'Content-Type': 'application/json'})
         # content = r.content  # utf-8
         content = r.json()
         main_html = content.get('d').split('|')[1].replace('', '')
