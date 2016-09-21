917271975c04bdb6173e96dbfa8bfd976bc113d9
neo1218 <neo1218@yeah.net>
2016-09-21 23:09:42 +0800

    update ele dor

--- restccnu/spiders/meter_index.py
+++ restccnu/spiders/meter_index.py
@@ -29,9 +29,11 @@ def colour_meter_index():
                     # 国交bug般的存在...
                     building = file[0]
                     key = option.get('value').split('*')[-2][:-2]
-                    key = '国' + building + '-' + key
+                    key = u'国' + building + '-' + key
                 else:
                     key = option.get('value').split('*')[-2][:-2]  # dor
+                    key.replace("新", "")
+                    key.replace("新增", "")
                 value = option.get('value').split('*')[0]          # meter
                 if key in _meter_index.keys():
                     _meter_index[key].append(value)
