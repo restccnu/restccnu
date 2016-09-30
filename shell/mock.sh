1d7c8338d007d731e2ac65959cf6331645184dd2
neo1218 <neo1218@yeah.net>
2016-09-30 23:53:44 +0800

    update mock sh

--- shell/mock.sh
+++ shell/mock.sh
@@ -1,6 +1,1 @@
-docker-compose build
-docker-compose stop restccnu_restccnu
-docker-compose stop restccnu_redis1
-docker-compose stop restccnu_redis2
-docker-compose up -d
-docker-compose logs
+supervisorctl restart restccnu
